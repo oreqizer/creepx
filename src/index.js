902a400df15fee0395c631cd54c25cc0636032e4
oreqizer <oreqizer@gmail.com>
2017-10-09 16:47:47 +0200

    index: Import the whole Rx for now

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import Observable from "rxjs/Observable";
+import Rx from "rxjs/Rx";
 import click from "./events/click";
 import doubleclick from "./events/doubleclick";
 import multiclick from "./events/multiclick";
 
 function creep(target, callback) {
-  const stream$ = Observable.fromEvent(target, "click");
+  const stream$ = Rx.Observable.fromEvent(target, "click");
 
-  Observable.merge(click(stream$), doubleclick(stream$), multiclick(stream$)).subscribe(callback);
+  Rx.Observable.merge(click(stream$), doubleclick(stream$), multiclick(stream$)).subscribe(callback);
 }
 
 export default creep;
