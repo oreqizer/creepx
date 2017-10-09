5a27811c2152682915ecef2ff222f80b7865cef5
oreqizer <oreqizer@gmail.com>
2017-10-09 18:25:47 +0200

    index: Prettier

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import multiclick from "./events/multiclick";
 function creep(target, callback) {
   const stream$ = Rx.Observable.fromEvent(target, "click");
 
-  Rx.Observable.merge(click(stream$), doubleclick(stream$), multiclick(stream$)).subscribe(callback);
+  Rx.Observable
+    .merge(click(stream$), doubleclick(stream$), multiclick(stream$))
+    .subscribe(callback);
 }
 
 export default creep;
