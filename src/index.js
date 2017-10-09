07abcfea9eefcb7b36da65556a8d07c001541910
oreqizer <oreqizer@gmail.com>
2017-10-09 16:42:27 +0200

    index: Allow custom target

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import click from "./events/click";
 import doubleclick from "./events/doubleclick";
 import multiclick from "./events/multiclick";
 
-function subscribe(callback) {
-  const stream$ = Observable.fromEvent(document, "click");
+function creep(target, callback) {
+  const stream$ = Observable.fromEvent(target, "click");
 
   Observable.merge(click(stream$), doubleclick(stream$), multiclick(stream$)).subscribe(callback);
 }
 
-export default subscribe;
+export default creep;
